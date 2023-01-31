bb2339a563facef72ea19aa1e6ac31cdd1dd3633
Esra Unal <gesraunal@gmail.com>
2023-01-31 20:10:40 +0100

    v2

--- src/Mywork.java
+++ src/Mywork.java
@@ -1,13 +1,8 @@
 public class Mywork {
 
-    git add.
+    git init
+    git add README.md
     git commit -m "first commit"
     git branch -M main
     git remote add origin https://github.com/esraunal07/Mywork.git
     git push -u origin main
-
-}
-
-
-        git branch -M main
-        git push -u origin main
